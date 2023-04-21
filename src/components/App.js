1aa5ac4abb84067ab1d40074e5d85137188d65cd
kelvin901 <wawerukelvin2019@gmail.com>
2023-04-21 16:50:06 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ function App() {
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
+  //function
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr} />
